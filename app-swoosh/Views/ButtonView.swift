ac90915c78baa02e1bfda554cce03b1554976d92
Ryan Salim <ryansalimm@gmail.com>
2017-09-20 19:51:38 +0700

    lesson-1

---
+++ app-swoosh/Views/ButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonView.swift
+//  app-swoosh
+//
+//  Created by admin on 9/20/17.
+//  Copyright © 2017 Ryan Salim. All rights reserved.
+//
+
+import UIKit
+
+class ButtonView: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
